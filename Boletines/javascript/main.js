a06a6df3cc08ce902f58ee69bef25e7c1c746f9b
Josinwua <Josinwua@gmail.com>
2021-02-19 17:40:24 -0500

    Redireccionamiento a Boletines

--- Boletines/javascript/main.js
+++ Boletines/javascript/main.js
@@ -1,18 +1,20 @@
 let links = document.querySelectorAll(".close");
-links.forEach(function(link){
-  link.addEventListener('click',function(ev){
+
+  links.forEach(function(link){
+
+    link.addEventListener('click',function(ev){
       ev.preventDefault();
-  let content = document.querySelector(".content")
+      let content = document.querySelector(".content")
 
-  content.classList.remove("animate__fadeIn");
-  content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeIn");
+        content.classList.remove("animate__animated");
 
-  content.classList.add("animate__fadeInDown");
-  content.classList.add("animate__animated");
+        content.classList.add("animate__fadeInDown");
+        content.classList.add("animate__animated");
 
-  setTimeout(function(){
-    location.href="/";
-  },600);
+        setTimeout(function(){
+          location.href="/Boletines";
+        },600);
 
   });
 });
